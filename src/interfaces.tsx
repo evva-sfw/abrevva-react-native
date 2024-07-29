436c12bf624efcbccd694bf577c3d0e2c4591d32
Hochstöger Matthias <116495532+mhochsto@users.noreply.github.com>
2024-07-29 13:31:58 +0200

    refactor: added interfaces and restructured app accordingly

--- src/interfaces.tsx
+++ src/interfaces.tsx
@@ -1,7 +1,3 @@
-export interface InitializeOptions {
-  androidNeverForLocation?: boolean;
-}
-
 export enum ScanMode {
   SCAN_MODE_LOW_POWER = 0,
   SCAN_MODE_BALANCED = 1,
@@ -91,4 +87,69 @@ export interface DisengageOptions {
   mobileGroupId: string;
   mobileAccessData: string;
   isPermanentRelease: boolean;
+}
+
+export interface AbrevvaNfcInterface {
+  connect: () => Promise<void>;
+  disconnect: () => Promise<void>;
+  read: () => Promise<void>;
+}
+export interface AbrevvaCryptoInterface {
+  encrypt: (
+    key: string,
+    iv: string,
+    adata: string,
+    pt: string,
+    tagLength?: number,
+  ) => Promise<Object>;
+  decrypt: (
+    key: string,
+    iv: string,
+    adata: string,
+    ct: string,
+    tagLength?: number,
+  ) => Promise<Object>;
+  generateKeyPair: () => Promise<Object>;
+  computeSharedSecret: (key: string, peerPublicKey: string) => Promise<Object>;
+  encryptFile: (sharedSecret: string, ptPath: string, ctPath: string) => Promise<Object>;
+  decryptFile: (sharedSecret: string, ptPath: string, ctPath: string) => Promise<Object>;
+  decryptFileFromURL: (sharedSecret: string, ptPath: string, url: string) => Promise<Object>;
+  random: (numBytes: number) => Promise<Object>;
+  derive: (key: string, salt: string, info: string, length: number) => Promise<Object>;
+}
+export interface AbrevvaBLEInterface {
+  initialize(androidNeverForLocation?: boolean): Promise<void>;
+  isEnabled(): Promise<boolean>;
+  isLocationEnabled(): Promise<boolean>;
+  startEnabledNotifications(callback: (result: boolean) => void): Promise<void>;
+  stopEnabledNotifications(): Promise<void>;
+  openLocationSettings(): Promise<void>;
+  openBluetoothSettings(): Promise<void>;
+  openAppSettings(): Promise<void>;
+  requestLEScan(
+    onScanResultCallback: (result: ScanResult) => void,
+    onConnectCallback: (address: string) => void,
+    onDisconnectCallback: (address: string) => void,
+    services?: string[],
+    name?: string,
+    namePrefix?: string,
+    optionalServices?: string[],
+    allowDuplicates?: boolean,
+    scanMode?: ScanMode,
+    timeout?: number,
+  ): Promise<void>;
+  stopLEScan(): Promise<void>;
+  connect(options: DeviceIdOptions & TimeoutOptions): Promise<void>;
+  disconnect(options: DeviceIdOptions): Promise<void>;
+  read(options: ReadOptions & TimeoutOptions): Promise<ReadResult>;
+  write(options: WriteOptions & TimeoutOptions): Promise<void>;
+  signalize(options: SignalizeOptions): Promise<void>;
+  disengage(options: DisengageOptions): Promise<StringResult>;
+  startNotifications(
+    deviceId: string,
+    service: string,
+    characteristic: string,
+    callback: (event: ReadResult) => void,
+  ): Promise<void>;
+  stopNotifications(options: ReadOptions): Promise<void>;
 }
