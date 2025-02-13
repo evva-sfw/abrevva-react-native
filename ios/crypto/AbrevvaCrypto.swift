ddbd3719b48ed64ff241026287680b533ec28e03
mhochsto <116495532+mhochsto@users.noreply.github.com>
2025-02-13 12:53:35 +0100

    chore: more linting

--- ios/crypto/AbrevvaCrypto.swift
+++ ios/crypto/AbrevvaCrypto.swift
@@ -5,238 +5,243 @@ import Foundation
 
 @objc(AbrevvaCrypto)
 public class AbrevvaCrypto: NSObject {
-  private let X25519Impl = X25519()
-  private let AesGcmImpl = AesGcm()
-  private let AesCcmImpl = AesCcm()
-  private let SimpleSecureRandomImpl = SimpleSecureRandom()
-  private let HKDFImpl = HKDFWrapper()
+    private let X25519Impl = X25519()
+    private let AesGcmImpl = AesGcm()
+    private let AesCcmImpl = AesCcm()
+    private let SimpleSecureRandomImpl = SimpleSecureRandom()
+    private let HKDFImpl = HKDFWrapper()
 
-  @objc func encrypt(
-    _ options: NSDictionary, resolver resolve: @escaping RCTPromiseResolveBlock,
-    rejecter reject: @escaping RCTPromiseRejectBlock
-  ) {
-    guard let optionsSwift = options as? [String: Any] else {
-      reject("Failed to convert NSDictionary to Swift dictionary", nil, nil)
-      return
+    @objc func encrypt(
+        _ options: NSDictionary, resolver resolve: @escaping RCTPromiseResolveBlock,
+        rejecter reject: @escaping RCTPromiseRejectBlock
+    ) {
+        guard let optionsSwift = options as? [String: Any] else {
+            reject("Failed to convert NSDictionary to Swift dictionary", nil, nil)
+            return
+        }
+
+        let key = [UInt8](hex: "0x" + (optionsSwift["key"] as? String ?? ""))
+        let iv = [UInt8](hex: "0x" + (optionsSwift["iv"] as? String ?? ""))
+        let adata = [UInt8](hex: "0x" + (optionsSwift["adata"] as? String ?? ""))
+        let pt = [UInt8](hex: "0x" + (optionsSwift["pt"] as? String ?? ""))
+        let tagLength = optionsSwift["tagLength"] as? Int ?? 0
+        let ct = AesCcmImpl.encrypt(
+            key: key, iv: iv, adata: adata, pt: pt, tagLength: tagLength
+        )
+        if ct.isEmpty {
+            reject("encrypt failed", nil, nil)
+        } else {
+            resolve([
+                "cipherText": [UInt8](ct[..<pt.count]).toHexString(),
+                "authTag": [UInt8](ct[pt.count...]).toHexString(),
+            ])
+        }
     }
 
-    let key = [UInt8](hex: "0x" + (optionsSwift["key"] as? String ?? ""))
-    let iv = [UInt8](hex: "0x" + (optionsSwift["iv"] as? String ?? ""))
-    let adata = [UInt8](hex: "0x" + (optionsSwift["adata"] as? String ?? ""))
-    let pt = [UInt8](hex: "0x" + (optionsSwift["pt"] as? String ?? ""))
-    let tagLength = optionsSwift["tagLength"] as? Int ?? 0
-    let ct = AesCcmImpl.encrypt(
-      key: key, iv: iv, adata: adata, pt: pt, tagLength: tagLength)
-    if ct.isEmpty {
-      reject("encrypt failed", nil, nil)
-    } else {
-      resolve([
-        "cipherText": [UInt8](ct[..<pt.count]).toHexString(),
-        "authTag": [UInt8](ct[pt.count...]).toHexString(),
-      ])
-    }
-  }
+    @objc func decrypt(
+        _ options: NSDictionary, resolver resolve: @escaping RCTPromiseResolveBlock,
+        rejecter reject: @escaping RCTPromiseRejectBlock
+    ) {
+        guard let optionsSwift = options as? [String: Any] else {
+            reject("Failed to convert NSDictionary to Swift dictionary", nil, nil)
+            return
+        }
 
-  @objc func decrypt(
-    _ options: NSDictionary, resolver resolve: @escaping RCTPromiseResolveBlock,
-    rejecter reject: @escaping RCTPromiseRejectBlock
-  ) {
-    guard let optionsSwift = options as? [String: Any] else {
-      reject("Failed to convert NSDictionary to Swift dictionary", nil, nil)
-      return
-    }
+        let key = [UInt8](hex: "0x" + (optionsSwift["key"] as? String ?? ""))
+        let iv = [UInt8](hex: "0x" + (optionsSwift["iv"] as? String ?? ""))
+        let adata = [UInt8](hex: "0x" + (optionsSwift["adata"] as? String ?? ""))
+        let ct = [UInt8](hex: "0x" + (optionsSwift["ct"] as? String ?? ""))
+        let tagLength = optionsSwift["tagLength"] as? Int ?? 0
 
-    let key = [UInt8](hex: "0x" + (optionsSwift["key"] as? String ?? ""))
-    let iv = [UInt8](hex: "0x" + (optionsSwift["iv"] as? String ?? ""))
-    let adata = [UInt8](hex: "0x" + (optionsSwift["adata"] as? String ?? ""))
-    let ct = [UInt8](hex: "0x" + (optionsSwift["ct"] as? String ?? ""))
-    let tagLength = optionsSwift["tagLength"] as? Int ?? 0
-
-    let pt = AesCcmImpl.decrypt(
-      key: key, iv: iv, adata: adata, ct: ct, tagLength: tagLength
-    ).toHexString()
-    if pt.isEmpty {
-      reject("decrypt failed", nil, nil)
-    } else {
-      resolve([
-        "plainText": pt,
-        "authOk": true,
-      ])
-    }
-  }
-
-  @objc func generateKeyPair(
-    _ resolve: @escaping RCTPromiseResolveBlock,
-    rejecter reject: @escaping RCTPromiseRejectBlock
-  ) {
-    let keyPair = X25519Impl.generateKeyPair()
-
-    resolve([
-      "privateKey": keyPair[0].toHexString(),
-      "publicKey": keyPair[1].toHexString(),
-    ])
-  }
-
-  @objc func computeSharedSecret(
-    _ options: NSDictionary, resolver resolve: @escaping RCTPromiseResolveBlock,
-    rejecter reject: @escaping RCTPromiseRejectBlock
-  ) {
-    guard let optionsSwift = options as? [String: Any] else {
-      reject("Failed to convert NSDictionary to Swift dictionary", nil, nil)
-      return
+        let pt = AesCcmImpl.decrypt(
+            key: key, iv: iv, adata: adata, ct: ct, tagLength: tagLength
+        ).toHexString()
+        if pt.isEmpty {
+            reject("decrypt failed", nil, nil)
+        } else {
+            resolve([
+                "plainText": pt,
+                "authOk": true,
+            ])
+        }
     }
 
-    let privateKeyData = Data(
-      hex: "0x" + (optionsSwift["key"] as? String ?? ""))
-    let publicKeyData = Data(
-      hex: "0x" + (optionsSwift["peerPublicKey"] as? String ?? ""))
-    let sharedSecret = X25519Impl.computeSharedSecret(
-      privateKeyData: privateKeyData, publicKeyData: publicKeyData)
+    @objc func generateKeyPair(
+        _ resolve: @escaping RCTPromiseResolveBlock,
+        rejecter _: @escaping RCTPromiseRejectBlock
+    ) {
+        let keyPair = X25519Impl.generateKeyPair()
 
-    resolve([
-      "sharedSecret": sharedSecret?.toHexString()
-    ])
-  }
-
-  @objc func encryptFile(
-    _ options: NSDictionary, resolver resolve: @escaping RCTPromiseResolveBlock,
-    rejecter reject: @escaping RCTPromiseRejectBlock
-  ) {
-    guard let optionsSwift = options as? [String: Any] else {
-      reject("Failed to convert NSDictionary to Swift dictionary", nil, nil)
-      return
+        resolve([
+            "privateKey": keyPair[0].toHexString(),
+            "publicKey": keyPair[1].toHexString(),
+        ])
     }
 
-    let sharedSecret = [UInt8](
-      hex: "0x" + (optionsSwift["sharedSecret"] as? String ?? ""))
-    let ptPath = optionsSwift["ptPath"] as? String ?? ""
-    let ctPath = optionsSwift["ctPath"] as? String ?? ""
+    @objc func computeSharedSecret(
+        _ options: NSDictionary, resolver resolve: @escaping RCTPromiseResolveBlock,
+        rejecter reject: @escaping RCTPromiseRejectBlock
+    ) {
+        guard let optionsSwift = options as? [String: Any] else {
+            reject("Failed to convert NSDictionary to Swift dictionary", nil, nil)
+            return
+        }
 
-    let operationResult = AesGcmImpl.encryptFile(
-      key: sharedSecret, pathPt: ptPath, pathCt: ctPath)
-    if operationResult == false {
-      reject("encryption failed", nil, nil)
-    } else {
-      resolve([
-        "opOk": operationResult
-      ])
-    }
-  }
+        let privateKeyData = Data(
+            hex: "0x" + (optionsSwift["key"] as? String ?? ""))
+        let publicKeyData = Data(
+            hex: "0x" + (optionsSwift["peerPublicKey"] as? String ?? ""))
+        let sharedSecret = X25519Impl.computeSharedSecret(
+            privateKeyData: privateKeyData, publicKeyData: publicKeyData
+        )
 
-  @objc func decryptFile(
-    _ options: NSDictionary, resolver resolve: @escaping RCTPromiseResolveBlock,
-    rejecter reject: @escaping RCTPromiseRejectBlock
-  ) {
-    guard let optionsSwift = options as? [String: Any] else {
-      reject("Failed to convert NSDictionary to Swift dictionary", nil, nil)
-      return
+        resolve([
+            "sharedSecret": sharedSecret?.toHexString(),
+        ])
     }
 
-    let sharedSecret = [UInt8](
-      hex: "0x" + (optionsSwift["sharedSecret"] as? String ?? ""))
-    let ctPath = optionsSwift["ctPath"] as? String ?? ""
-    let ptPath = optionsSwift["ptPath"] as? String ?? ""
+    @objc func encryptFile(
+        _ options: NSDictionary, resolver resolve: @escaping RCTPromiseResolveBlock,
+        rejecter reject: @escaping RCTPromiseRejectBlock
+    ) {
+        guard let optionsSwift = options as? [String: Any] else {
+            reject("Failed to convert NSDictionary to Swift dictionary", nil, nil)
+            return
+        }
 
-    let url = URL(fileURLWithPath: ctPath)
+        let sharedSecret = [UInt8](
+            hex: "0x" + (optionsSwift["sharedSecret"] as? String ?? ""))
+        let ptPath = optionsSwift["ptPath"] as? String ?? ""
+        let ctPath = optionsSwift["ctPath"] as? String ?? ""
 
-    let data: Data
-    do {
-      data = try Data(contentsOf: url, options: .mappedIfSafe)
-    } catch {
-      debugPrint(
-        "\(AbrevvaCrypto.description()): Failed to load data from file")
-      return reject("Failed to load data", nil, nil)
+        let operationResult = AesGcmImpl.encryptFile(
+            key: sharedSecret, pathPt: ptPath, pathCt: ctPath
+        )
+        if operationResult == false {
+            reject("encryption failed", nil, nil)
+        } else {
+            resolve([
+                "opOk": operationResult,
+            ])
+        }
     }
 
-    let operationResult = AesGcmImpl.decryptFile(
-      key: sharedSecret, data: data, pathPt: ptPath)
-    if operationResult == false {
-      reject("Encryption has failed", nil, nil)
-    } else {
-      resolve([
-        "opOk": operationResult
-      ])
-    }
-  }
+    @objc func decryptFile(
+        _ options: NSDictionary, resolver resolve: @escaping RCTPromiseResolveBlock,
+        rejecter reject: @escaping RCTPromiseRejectBlock
+    ) {
+        guard let optionsSwift = options as? [String: Any] else {
+            reject("Failed to convert NSDictionary to Swift dictionary", nil, nil)
+            return
+        }
 
-  @objc func decryptFileFromURL(
-    _ options: NSDictionary, resolver resolve: @escaping RCTPromiseResolveBlock,
-    rejecter reject: @escaping RCTPromiseRejectBlock
-  ) {
-    guard let optionsSwift = options as? [String: Any] else {
-      reject("Failed to convert NSDictionary to Swift dictionary", nil, nil)
-      return
-    }
+        let sharedSecret = [UInt8](
+            hex: "0x" + (optionsSwift["sharedSecret"] as? String ?? ""))
+        let ctPath = optionsSwift["ctPath"] as? String ?? ""
+        let ptPath = optionsSwift["ptPath"] as? String ?? ""
 
-    let sharedSecret = [UInt8](
-      hex: "0x" + (optionsSwift["sharedSecret"] as? String ?? ""))
-    let ptPath = optionsSwift["ptPath"] as? String ?? ""
+        let url = URL(fileURLWithPath: ctPath)
 
-    let url = URL(string: optionsSwift["url"] as? String ?? "")
+        let data: Data
+        do {
+            data = try Data(contentsOf: url, options: .mappedIfSafe)
+        } catch {
+            debugPrint(
+                "\(AbrevvaCrypto.description()): Failed to load data from file")
+            return reject("Failed to load data", nil, nil)
+        }
 
-    let data: Data
-    do {
-      data = try Data(contentsOf: url!)
-    } catch {
-      debugPrint("\(AbrevvaCrypto.description()): Failed to load data from url")
-      return reject("Failed to load data", nil, nil)
-    }
-
-    let operationResult = AesGcmImpl.decryptFile(
-      key: sharedSecret, data: data, pathPt: ptPath)
-    if operationResult == false {
-      reject("Decryption has failed", nil, nil)
-    } else {
-      resolve([
-        "opOk": operationResult
-      ])
-    }
-  }
-
-  @objc func random(
-    _ options: NSDictionary, resolver resolve: @escaping RCTPromiseResolveBlock,
-    rejecter reject: @escaping RCTPromiseRejectBlock
-  ) {
-    guard let optionsSwift = options as? [String: Any] else {
-      reject("Failed to convert NSDictionary to Swift dictionary", nil, nil)
-      return
+        let operationResult = AesGcmImpl.decryptFile(
+            key: sharedSecret, data: data, pathPt: ptPath
+        )
+        if operationResult == false {
+            reject("Encryption has failed", nil, nil)
+        } else {
+            resolve([
+                "opOk": operationResult,
+            ])
+        }
     }
 
-    let numBytes = optionsSwift["numBytes"] as? Int ?? 0
+    @objc func decryptFileFromURL(
+        _ options: NSDictionary, resolver resolve: @escaping RCTPromiseResolveBlock,
+        rejecter reject: @escaping RCTPromiseRejectBlock
+    ) {
+        guard let optionsSwift = options as? [String: Any] else {
+            reject("Failed to convert NSDictionary to Swift dictionary", nil, nil)
+            return
+        }
 
-    let rnd = SimpleSecureRandomImpl.random(numBytes).toHexString()
-    if rnd.isEmpty {
-      reject("random generation failed", nil, nil)
-    } else {
-      resolve([
-        "value": rnd
-      ])
+        let sharedSecret = [UInt8](
+            hex: "0x" + (optionsSwift["sharedSecret"] as? String ?? ""))
+        let ptPath = optionsSwift["ptPath"] as? String ?? ""
+
+        let url = URL(string: optionsSwift["url"] as? String ?? "")
+
+        let data: Data
+        do {
+            data = try Data(contentsOf: url!)
+        } catch {
+            debugPrint("\(AbrevvaCrypto.description()): Failed to load data from url")
+            return reject("Failed to load data", nil, nil)
+        }
+
+        let operationResult = AesGcmImpl.decryptFile(
+            key: sharedSecret, data: data, pathPt: ptPath
+        )
+        if operationResult == false {
+            reject("Decryption has failed", nil, nil)
+        } else {
+            resolve([
+                "opOk": operationResult,
+            ])
+        }
     }
-  }
 
-  @objc func derive(
-    _ options: NSDictionary, resolver resolve: @escaping RCTPromiseResolveBlock,
-    rejecter reject: @escaping RCTPromiseRejectBlock
-  ) {
-    guard let optionsSwift = options as? [String: Any] else {
-      reject("Failed to convert NSDictionary to Swift dictionary", nil, nil)
-      return
+    @objc func random(
+        _ options: NSDictionary, resolver resolve: @escaping RCTPromiseResolveBlock,
+        rejecter reject: @escaping RCTPromiseRejectBlock
+    ) {
+        guard let optionsSwift = options as? [String: Any] else {
+            reject("Failed to convert NSDictionary to Swift dictionary", nil, nil)
+            return
+        }
+
+        let numBytes = optionsSwift["numBytes"] as? Int ?? 0
+
+        let rnd = SimpleSecureRandomImpl.random(numBytes).toHexString()
+        if rnd.isEmpty {
+            reject("random generation failed", nil, nil)
+        } else {
+            resolve([
+                "value": rnd,
+            ])
+        }
     }
 
-    let key = [UInt8](hex: "0x" + (optionsSwift["key"] as? String ?? ""))
-    let salt = [UInt8](hex: "0x" + (optionsSwift["salt"] as? String ?? ""))
-    let info = [UInt8](hex: "0x" + (optionsSwift["info"] as? String ?? ""))
-    let length = optionsSwift["length"] as? Int ?? 0
+    @objc func derive(
+        _ options: NSDictionary, resolver resolve: @escaping RCTPromiseResolveBlock,
+        rejecter reject: @escaping RCTPromiseRejectBlock
+    ) {
+        guard let optionsSwift = options as? [String: Any] else {
+            reject("Failed to convert NSDictionary to Swift dictionary", nil, nil)
+            return
+        }
 
-    let derived = HKDFImpl.derive(
-      key: key, salt: salt, info: info, length: length
-    ).toHexString()
-    if derived.isEmpty {
-      reject("derivation failed", nil, nil)
-    } else {
-      resolve([
-        "value": derived
-      ])
+        let key = [UInt8](hex: "0x" + (optionsSwift["key"] as? String ?? ""))
+        let salt = [UInt8](hex: "0x" + (optionsSwift["salt"] as? String ?? ""))
+        let info = [UInt8](hex: "0x" + (optionsSwift["info"] as? String ?? ""))
+        let length = optionsSwift["length"] as? Int ?? 0
+
+        let derived = HKDFImpl.derive(
+            key: key, salt: salt, info: info, length: length
+        ).toHexString()
+        if derived.isEmpty {
+            reject("derivation failed", nil, nil)
+        } else {
+            resolve([
+                "value": derived,
+            ])
+        }
     }
-  }
 }
